0066c6c02a7311fd10a9599633c9cfdb0bfdc358
Swasthik G <161289221+swasthikg73@users.noreply.github.com>
2025-05-15 07:01:12 +0000

    Index.js file updated

--- server/index.js
+++ server/index.js
@@ -1,63 +1,63 @@
-// const express = require("express");
-// var cors = require("cors");
-// const { createServer } = require("node:http");
-// const userRouter = require("./routes/userRoutes");
-// const budgetRouter = require("./routes/budgetRoute");
-// const { error } = require("node:console");
-
-// const PORT = 3000;
-
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-
-// const server = createServer(app);
-
-// app.get("", (req, res) => {
-//   res.send({
-//   activeStatus :true,
-//   error:false
-//   })
-// });
-
-// app.get("/", (req, res)=> {
-// res.send({
-//   activeStatus :true,
-//   error:false
-// });
-// })
-
-// app.use("/user", userRouter);
-// app.use("/budget", budgetRouter);
-
-// server.listen(PORT, () => {
-//   try {
-//     console.log("App is running on port :", `http://localhost:${PORT}`);
-//   } catch (err) {
-//     console.log("Error occured: ", err);
-//   }
-// })
-
 const express = require("express");
-const serverless = require("serverless-http"); // Wraps Express for serverless
-const cors = require("cors");
-
+var cors = require("cors");
+const { createServer } = require("node:http");
 const userRouter = require("./routes/userRoutes");
 const budgetRouter = require("./routes/budgetRoute");
+const { error } = require("node:console");
+
+const PORT = 3000;
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ activeStatus: true, error: false });
+const server = createServer(app);
+
+app.get("", (req, res) => {
+  res.send({
+  activeStatus :true,
+  error:false
+  })
 });
+
+app.get("/", (req, res)=> {
+res.send({
+  activeStatus :true,
+  error:false
+});
+})
 
 app.use("/user", userRouter);
 app.use("/budget", budgetRouter);
 
-module.exports.handler = serverless(app);
+server.listen(PORT, () => {
+  try {
+    console.log("App is running on port :", `http://localhost:${PORT}`);
+  } catch (err) {
+    console.log("Error occured: ", err);
+  }
+})
+
+// const express = require("express");
+// const serverless = require("serverless-http"); // Wraps Express for serverless
+// const cors = require("cors");
+
+// const userRouter = require("./routes/userRoutes");
+// const budgetRouter = require("./routes/budgetRoute");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.get("/", (req, res) => {
+//   res.json({ activeStatus: true, error: false });
+// });
+
+// app.use("/user", userRouter);
+// app.use("/budget", budgetRouter);
+
+// module.exports.handler = serverless(app);
 
